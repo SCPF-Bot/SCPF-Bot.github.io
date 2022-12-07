d0c55b1bfd670f5b8eeba5a7083fd1e43703474a
ANGIE DELA MANDEVILLA <unbug.ph@gmail.com>
2022-12-07 12:26:22 +0800

    Delete download.files.py

--- pages/changelog/download.files.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url1 = "https://raw.githubusercontent.com/revanced/revanced-patches/main/CHANGELOG.md"
-myfile1 = requests.get(url1, allow_redirects=True)
-
-open("temp/patches.md", "wb").write(myfile1.content)
