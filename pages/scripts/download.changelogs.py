2380c9b044beba9e7dd791de79ab2ff55b11327e
ANGIE DELA MANDEVILLA <unbug.ph@gmail.com>
2022-12-06 18:20:24 -0900

    Update download.changelogs.py

--- pages/scripts/download.changelogs.py
+++ pages/scripts/download.changelogs.py
@@ -3,4 +3,4 @@ import requests
 url = "https://raw.githubusercontent.com/revanced/revanced-patches/main/CHANGELOG.md"
 myfile = requests.get(url, allow_redirects=True)
 
-open("changelogs/patches.changelog.md", "wb").write(myfile.content)
+open("temp/patches.changelog.md", "wb").write(myfile.content)
