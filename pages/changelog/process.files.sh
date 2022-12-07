60c0ea477d5f8f8e201e21053457ed67fdd6e126
ANGIE DELA MANDEVILLA <unbug.ph@gmail.com>
2022-12-07 15:32:21 +1100

    Update process.files.sh

--- pages/changelog/process.files.sh
+++ pages/changelog/process.files.sh
@@ -5,6 +5,7 @@ mkdir -p temp
 wget -P temp -O patches.md https://raw.githubusercontent.com/revanced/revanced-patches/main/CHANGELOG.md
 
 rm -rf pages
+
 mkdir -p pages
 
 cat origin/origin.md temp/patches.md > pages/patches.md
