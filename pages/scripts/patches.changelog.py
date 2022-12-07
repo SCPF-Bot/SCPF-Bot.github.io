bc9fad80e5dc0bb2eb6d2c2de2612c5651a78a50
ANGIE DELA MANDEVILLA <unbug.ph@gmail.com>
2022-12-07 04:19:58 +0200

    Update patches.changelog.py

--- pages/scripts/patches.changelog.py
+++ pages/scripts/patches.changelog.py
@@ -3,4 +3,4 @@ import requests
 url = "https://raw.githubusercontent.com/revanced/revanced-patches/main/CHANGELOG.md"
 myfile = requests.get(url, allow_redirects=True)
 
-open("c:/users/LikeGeeks/documents/hello.pdf", "wb").write(myfile.content)
+open("patches.changelog.md", "wb").write(myfile.content)
