ea70107be61928b2daa592004c099b4d03ccfa5b
ANGIE DELA MANDEVILLA <unbug.ph@gmail.com>
2022-12-07 05:59:03 +0200

    Update download.files.py

--- pages/changelog/download.files.py
+++ pages/changelog/download.files.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://raw.githubusercontent.com/revanced/revanced-patches/main/CHANGELOG.md"
-myfile = requests.get(url, allow_redirects=True)
+url-1 = "https://raw.githubusercontent.com/revanced/revanced-patches/main/CHANGELOG.md"
+myfile-1 = requests.get(url-1, allow_redirects=True)
 
-open("temp/patches.md", "wb").write(myfile.content)
+open("temp/patches.md", "wb").write(myfile-1.content)
